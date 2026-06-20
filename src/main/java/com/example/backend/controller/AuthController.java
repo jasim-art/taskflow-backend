2b814b20a5490132e91098c67fd33909ec1aca7f
jasim-art <msjasim959@gmail.com>
2026-06-20 08:56:57 +0530

    Update AuthController

--- src/main/java/com/example/backend/controller/AuthController.java
+++ src/main/java/com/example/backend/controller/AuthController.java
@@ -4,12 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.entity.User;
@@ -18,7 +17,6 @@ import com.example.backend.service.UserService;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final UserService userService;
@@ -71,9 +69,9 @@ public class AuthController {
         return response;
     }
 
-    @GetMapping("/profile")
+    @GetMapping("/profile/{email}")
     public User getProfile(
-            @RequestParam String email) {
+            @PathVariable String email) {
 
         return userRepository
                 .findByEmail(email)
